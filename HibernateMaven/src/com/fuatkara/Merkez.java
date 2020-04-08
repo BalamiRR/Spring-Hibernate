16eda8f4164d9f11afb063d3cab9aa7c8ad98005
Fuat KARA <fuatkara.95@gmail.com>
2020-04-08 23:34:08 +0200

    Hibernate and PostgreSQL example

---
+++ HibernateMaven/src/com/fuatkara/Merkez.java
@@ -0,0 +1,23 @@
+package com.fuatkara;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Merkez {
+	public static void main(String[] args) {
+		
+		Personel personel = new Personel();
+		
+		personel.setPersonelName("Fuat");
+		personel.setPersonelSurname("Kara");
+		personel.setPersonelEmail("abbbb");
+		
+		//--------------------------------------------------
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(personel);
+		session.getTransaction().commit();
+	}
+}
